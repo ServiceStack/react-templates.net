59f176087678ea9ab1f1ffac7e2da0b31d358ca4
Demis Bellot <demis.bellot@gmail.com>
2026-02-15 13:34:56 +0800

    Add favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,21 @@ import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
 import { Inter } from 'next/font/google';
 import Script from 'next/script';
+import type { Metadata } from 'next';
 
 const inter = Inter({
   subsets: ['latin'],
 });
+
+export const metadata: Metadata = {
+  icons: {
+    apple: '/apple-touch-icon.png',
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+  },
+};
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
